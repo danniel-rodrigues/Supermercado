e245f26b57c6007d411ac7ccb913077dcee30f30
Daniel Rodrigues <96748212+danniel-rodrigues@users.noreply.github.com>
2024-04-02 16:01:56 -0300

    Ajuste de tamanho da tela Demitir Funcionário.

--- src/main/java/Views/TelaDemitirFuncionario.java
+++ src/main/java/Views/TelaDemitirFuncionario.java
@@ -75,7 +75,7 @@ public class TelaDemitirFuncionario{
         Insets padding = new Insets(60);
         vbox.setPadding(padding);
 
-        Scene scene = new Scene(vbox, 600, 500);
+        Scene scene = new Scene(vbox, 600, 350);
         stage.setTitle("Supermercado - Demitir Funcionário");
         stage.setScene(scene);
         stage.show();

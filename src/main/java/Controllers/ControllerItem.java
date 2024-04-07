2658bebbe9f9e09970b998bce8813090032f6540
Daniel Rodrigues <96748212+danniel-rodrigues@users.noreply.github.com>
2024-04-06 22:29:53 -0300

    Corrigido bug de código de produto nulo ao tentar cadastrar um item.

--- src/main/java/Controllers/ControllerItem.java
+++ src/main/java/Controllers/ControllerItem.java
@@ -60,14 +60,19 @@ public class ControllerItem {
     }
 
     public void adicionarItem(Item item) {
-        if(ItemDAO.adicionarItem(item)) {
-            viewCadastroItem.getResposta().setText("Item cadastrado com sucesso!");
-            viewCadastroItem.getResposta().setStyle("-fx-text-fill: green;");
+        try {
+            if(ItemDAO.adicionarItem(item)) {
+                viewCadastroItem.getResposta().setText("Item cadastrado com sucesso!");
+                viewCadastroItem.getResposta().setStyle("-fx-text-fill: green;");
 
-            // Limpa os campos de entrada após o cadastro
-            viewCadastroItem.limparCampos();
-        } else {
-            viewCadastroItem.getResposta().setText("Erro na inserção!");
+                // Limpa os campos de entrada após o cadastro
+                viewCadastroItem.limparCampos();
+            } else {
+                viewCadastroItem.getResposta().setText("Erro na inserção!");
+                viewCadastroItem.getResposta().setStyle("-fx-text-fill: red;");
+            }
+        } catch (NullPointerException e) {
+            viewCadastroItem.getResposta().setText("Erro: Código do produto não existe!");
             viewCadastroItem.getResposta().setStyle("-fx-text-fill: red;");
         }
     }
